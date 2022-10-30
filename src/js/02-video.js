d5939417f065e70a786df27322dfd9bd1bd1f860
LenaDerevyanko <106963708+LenaDerevyanko@users.noreply.github.com>
2022-10-30 21:57:17 +0200

    up

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,14 +1,35 @@
+import Player from '@vimeo/player';
 import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const LOCALSTORAGE_KEY = "videoplayer-current-time";
+const player = new Player(iframe);
+
 
 player.on('timeupdate', throttle(onPlay, 1000));
+function onPlay(event) {
+   localStorage.setItem(LOCALSTORAGE_KEY, event.seconds);
+};
 
-function onPlay({ seconds }) {
-  localStorage.setItem('videoplayer-current-time', seconds);
-}
+const getLocalStorageValue = localStorage.getItem(LOCALSTORAGE_KEY); 
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+if (getLocalStorageValue === null || getLocalStorageValue === undefined) {
+    return ;
+} else {
+    player.setCurrentTime(getLocalStorageValue);
+};
 
 
+
+// import throttle from 'lodash.throttle';
+
+// const iframe = document.querySelector('iframe');
+// const player = new Vimeo.Player(iframe);
+
+// player.on('timeupdate', throttle(onPlay, 1000));
+
+// function onPlay({ seconds }) {
+//   localStorage.setItem('videoplayer-current-time', seconds);
+// }
+
+// player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
