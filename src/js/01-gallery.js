3b42830a2a6299c2980e2b5afb692aeb59644f33
LenaDerevyanko <106963708+LenaDerevyanko@users.noreply.github.com>
2022-10-30 12:04:42 +0200

    up

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,40 @@
 // Add imports above this line
-import { galleryItems } from './gallery-items';
+// import { galleryItems } from './gallery-items';
 // Change code below this line
 
+// console.log(galleryItems);
+
+
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+import { galleryItems } from './gallery-items.js';
+
+
 console.log(galleryItems);
+
+
+const galleryContainer = document.querySelector('.gallery');
+
+galleryContainer.insertAdjacentHTML('beforeend',createGalleryItems(galleryItems))
+
+galleryContainer.addEventListener('click', onImageClick )
+
+new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
+
+function onImageClick(event) {
+    event.preventDefault();
+    
+    if(!event.target.classList.contains('gallery__image')){
+        return;
+    }
+
+}
+function createGalleryItems(galleryItems) {
+    return galleryItems.map(({preview , original , description }) => { 
+        return `<a class="gallery__item" href="${original}">
+       <img class="gallery__image" src="${preview}" alt="${description}" />
+     </a>`}).join('');   
+}
